e77ee7fbe4ba05a37c35e60d32fce18e7531ce06
DESKTOP-8U0R2CQ\User <madyoung77@gmail.com>
2022-02-09 18:02:55 +0900

    bean 설정 추가

--- src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
+++ src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
@@ -2,11 +2,13 @@ package com.example.kafkaexample.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configurable
 public class KafkaTopicConfig {
 
+    @Bean
     public NewTopic doyoungcodeTopic() {
         return TopicBuilder.name("doyoungcode")
                 .build();
